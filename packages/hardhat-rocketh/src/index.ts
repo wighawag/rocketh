569f0f1c5ab9abbabe1c1a7c7f75cd020a0616f7
wighawag <wighawag@gmail.com>
2023-06-26 14:40:40 +0100

    generator folder for artifacts

--- packages/hardhat-rocketh/src/index.ts
+++ packages/hardhat-rocketh/src/index.ts
@@ -7,6 +7,7 @@ import {subtask, task, extendConfig} from 'hardhat/config';
 import {TASK_COMPILE_SOLIDITY_EMIT_ARTIFACTS} from 'hardhat/builtin-tasks/task-names';
 import {loadAndExecuteDeployments} from 'rocketh';
 import {HardhatConfig, HardhatUserConfig} from 'hardhat/types';
+import {ArtifactGenerationConfig} from './type-extensions';
 
 export * from './utils';
 
@@ -75,39 +76,111 @@ task('deploy', 'Deploy contracts').setAction(async (args, hre) => {
 	});
 });
 
+function traverse(
+	dir: string,
+	result: any[] = [],
+	topDir?: string,
+	filter?: (name: string, stats: any) => boolean // TODO any is Stats
+): Array<{
+	name: string;
+	path: string;
+	relativePath: string;
+	mtimeMs: number;
+	directory: boolean;
+}> {
+	fs.readdirSync(dir).forEach((name) => {
+		const fPath = path.resolve(dir, name);
+		const stats = fs.statSync(fPath);
+		if ((!filter && !name.startsWith('.')) || (filter && filter(name, stats))) {
+			const fileStats = {
+				name,
+				path: fPath,
+				relativePath: path.relative(topDir || dir, fPath),
+				mtimeMs: stats.mtimeMs,
+				directory: stats.isDirectory(),
+			};
+			if (fileStats.directory) {
+				result.push(fileStats);
+				return traverse(fPath, result, topDir || dir, filter);
+			}
+			result.push(fileStats);
+		}
+	});
+	return result;
+}
+
+function writeFiles(name: string | undefined, data: any, config: ArtifactGenerationConfig) {
+	const js = typeof config?.js === 'string' ? [config?.js] : config?.js || [];
+	const ts = typeof config?.ts === 'string' ? [config?.ts] : config?.ts || [];
+	const json = typeof config?.json === 'string' ? [config?.json] : config?.json || [];
+
+	if (typeof ts === 'object' && ts.length > 0) {
+		const newContent = `export default ${JSON.stringify(data, null, 2)} as const;`;
+		for (const tsFile of ts) {
+			const filepath = name ? `${tsFile.replace(/\.ts$/, '')}/${name}.ts` : tsFile;
+			const folderPath = path.dirname(filepath);
+			fs.mkdirSync(folderPath, {recursive: true});
+			fs.writeFileSync(filepath, newContent);
+		}
+	}
+
+	if (typeof js === 'object' && js.length > 0) {
+		const newContent = `export default /** @type {const} **/ (${JSON.stringify(data, null, 2)});`;
+		const dtsContent = `export = ${JSON.stringify(data, null, 2)} as const;`;
+		for (const jsFile of js) {
+			const filepath = name ? `${jsFile.replace(/\.js$/, '')}/${name}.js` : jsFile;
+			const folderPath = path.dirname(filepath);
+			fs.mkdirSync(folderPath, {recursive: true});
+			fs.writeFileSync(filepath, newContent);
+			fs.writeFileSync(filepath.replace(/\.js$/, '.d.ts'), dtsContent);
+		}
+	}
+
+	if (typeof json === 'object' && json.length > 0) {
+		const newContent = JSON.stringify(data, null, 2);
+		for (const jsonFile of json) {
+			const filepath = name ? `${jsonFile.replace(/\.json$/, '')}/${name}.json` : jsonFile;
+			const folderPath = path.dirname(filepath);
+			fs.mkdirSync(folderPath, {recursive: true});
+			fs.writeFileSync(filepath, newContent);
+		}
+	}
+}
+
 subtask(TASK_COMPILE_SOLIDITY_EMIT_ARTIFACTS).setAction(async (args, hre, runSuper): Promise<any> => {
 	const allArtifacts: {[name: string]: any} = {}; // TODO read current ?
 	const artifactResult = await runSuper(args);
 	const shortNameDict: {[shortName: string]: boolean} = {};
-	for (const artifact of artifactResult.artifactsEmittedPerFile) {
-		const filepath = `./artifacts/${artifact.file.sourceName}/${artifact.artifactsEmitted[0]}.json`;
-		if (fs.existsSync(filepath)) {
-			for (let i = 0; i < artifact.artifactsEmitted.length; i++) {
-				const shortName = artifact.artifactsEmitted[i];
-				const content = fs.readFileSync(filepath, 'utf-8');
-				const parsed = JSON.parse(content);
+	const files = traverse('./artifacts', [], undefined, (name) => name != 'build-info');
 
-				const debugFilepath = filepath.replace('.json', '.dbg.json');
-				const debugContent = fs.readFileSync(debugFilepath, 'utf-8');
-				const parsedDebug: {_format: string; buildInfo: string} = JSON.parse(debugContent);
-				const buildInfoFilepath = path.join(path.dirname(path.resolve(debugFilepath)), parsedDebug.buildInfo);
-				const buildInfoContent = fs.readFileSync(buildInfoFilepath, 'utf-8');
-				const parsedBuildInfo = JSON.parse(buildInfoContent);
-				const solidityOutput = parsedBuildInfo.output.contracts[artifact.file.sourceName][shortName];
+	for (const file of files) {
+		const filename = path.basename(file.path);
+		const dirname = path.dirname(file.relativePath);
+		// const namePath = dirname.replace('.sol', '');
+		const contractName = filename.replace('.json', '');
+		if (file.directory || file.path.endsWith('.dbg.json')) {
+			continue;
+		}
+		// const shortName = artifact.artifactsEmitted[i];
+		const content = fs.readFileSync(file.path, 'utf-8');
+		const parsed = JSON.parse(content);
 
-				const artifactObject = {...parsed, ...solidityOutput};
-				const fullName = `${artifact.file.sourceName}/${shortName}`;
-				allArtifacts[fullName] = artifactObject;
-				if (shortNameDict[shortName]) {
-					delete allArtifacts[shortName];
-				} else {
-					allArtifacts[shortName] = artifactObject;
-					shortNameDict[shortName] = true;
-				}
-			}
+		const debugFilepath = file.path.replace('.json', '.dbg.json');
+		const debugContent = fs.readFileSync(debugFilepath, 'utf-8');
+		const parsedDebug: {_format: string; buildInfo: string} = JSON.parse(debugContent);
+		const buildInfoFilepath = path.join(path.dirname(path.resolve(debugFilepath)), parsedDebug.buildInfo);
+		const buildInfoContent = fs.readFileSync(buildInfoFilepath, 'utf-8');
+		const parsedBuildInfo = JSON.parse(buildInfoContent);
+		const solidityOutput = parsedBuildInfo.output.contracts[dirname][contractName];
+
+		const artifactObject = {...parsed, ...solidityOutput};
+		const fullName = `${dirname}/${contractName}`;
+		allArtifacts[fullName] = artifactObject;
+		if (shortNameDict[contractName]) {
+			delete allArtifacts[contractName];
 		} else {
-			// this can happen for solidity file without contract exported, just error or types for example
-			// throw new Error(`no artifact at ${filepath}`);
+			allArtifacts[contractName] = artifactObject;
+			shortNameDict[contractName] = true;
 		}
 	}
 	for (const key of Object.keys(allArtifacts)) {
@@ -122,47 +195,11 @@ subtask(TASK_COMPILE_SOLIDITY_EMIT_ARTIFACTS).setAction(async (args, hre, runSup
 		}
 	}
 
-	const js =
-		typeof hre.config.generateArtifacts?.js === 'string'
-			? [hre.config.generateArtifacts?.js]
-			: hre.config.generateArtifacts?.js || [];
-	const ts =
-		typeof hre.config.generateArtifacts?.ts === 'string'
-			? [hre.config.generateArtifacts?.ts]
-			: hre.config.generateArtifacts?.ts || [];
-	const json =
-		typeof hre.config.generateArtifacts?.json === 'string'
-			? [hre.config.generateArtifacts?.json]
-			: hre.config.generateArtifacts?.json || [];
-
-	if (typeof ts === 'object' && ts.length > 0) {
-		const newContent = `export default ${JSON.stringify(allArtifacts, null, 2)} as const;`;
-		for (const tsFile of ts) {
-			const folderPath = path.dirname(tsFile);
-			fs.mkdirSync(folderPath, {recursive: true});
-			fs.writeFileSync(tsFile, newContent);
-		}
+	for (const key of Object.keys(allArtifacts)) {
+		const artifact = allArtifacts[key];
+		writeFiles(key, artifact, hre.config.generateArtifacts);
 	}
-
-	if (typeof js === 'object' && js.length > 0) {
-		const newContent = `export default /** @type {const} **/ (${JSON.stringify(allArtifacts, null, 2)});`;
-		const dtsContent = `export = ${JSON.stringify(allArtifacts, null, 2)} as const;`;
-		for (const jsFile of js) {
-			const folderPath = path.dirname(jsFile);
-			fs.mkdirSync(folderPath, {recursive: true});
-			fs.writeFileSync(jsFile, newContent);
-			fs.writeFileSync(jsFile.replace('.js', '.d.ts'), dtsContent);
-		}
-	}
-
-	if (typeof json === 'object' && json.length > 0) {
-		const newContent = JSON.stringify(allArtifacts, null, 2);
-		for (const jsonFile of json) {
-			const folderPath = path.dirname(jsonFile);
-			fs.mkdirSync(folderPath, {recursive: true});
-			fs.writeFileSync(jsonFile, newContent);
-		}
-	}
+	// writeFiles(allArtifacts, hre.config.generateArtifacts);
 
 	return artifactResult;
 });
--- packages/hardhat-rocketh/src/type-extensions.ts
+++ packages/hardhat-rocketh/src/type-extensions.ts
@@ -1,20 +1,18 @@
 import 'hardhat/types/runtime';
 import 'hardhat/types/config';
 
+export type ArtifactGenerationConfig = {
+	js?: string[];
+	ts?: string[];
+	json?: string[];
+};
+
 declare module 'hardhat/types/config' {
 	interface HardhatUserConfig {
-		generateArtifacts?: {
-			js?: string[];
-			ts?: string[];
-			json?: string[];
-		};
+		generateArtifacts?: ArtifactGenerationConfig;
 	}
 
 	interface HardhatConfig {
-		generateArtifacts: {
-			js?: string[];
-			ts?: string[];
-			json?: string[];
-		};
+		generateArtifacts: ArtifactGenerationConfig;
 	}
 }
