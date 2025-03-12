d4908900df3821b2b4a2aa94cdb0cc4d659491e9
wighawag <wighawag@gmail.com>
2025-03-12 15:14:16 +0000

    remove hardhat-deploy plugin from this repo

--- packages/hardhat-deploy/src/config/default.ts
+++
@@ -1,9 +0,0 @@
-import type {ArtifactGenerationConfig} from '../types.js';
-
-export const DEFAULT_CONFIG: ArtifactGenerationConfig = {
-	js: [],
-	ts: ['./generated/artifacts.ts'],
-	json: [],
-	jsm: [],
-	tsm: [],
-};
