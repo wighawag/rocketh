a7b564cab992e5207fe806258a6ef301e07d4723
wighawag <wighawag@gmail.com>
2025-12-19 10:46:34 +0000

    fix

--- packages/rocketh/src/index.ts
+++ packages/rocketh/src/index.ts
@@ -9,6 +9,9 @@ export {
 } from './executor/index.js';
 export {createEnvironment, loadDeployments} from './environment/index.js';
 
+// used by hardhat-deploy and instead of having hardhat-deploy depends on @rocketh/core we export it here as well
+export {enhanceEnvIfNeeded} from '@rocketh/core/environment';
+
 export {
 	getChainConfigFromUserConfigAndDefaultChainInfo,
 	chainByCanonicalName,
